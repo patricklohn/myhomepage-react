959fd1b1fb3aa51003125e6f65240ffce6022daa
patricklohn <patricklohn3@gmail.com>
2025-03-21 14:27:00 -0300

    Ajustes de estilização e complemntação do componente

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,9 +7,22 @@ const Skills = () => {
         <h1>MySkills</h1>
         <div className='container-skills'>
             <div className='skill'>
-                <a href="#"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" alt="Logo Html" /></a>
-                <h4>Html:</h4>
-                <span>(HyperText Markup Language ou Linguagem de Marcação de Hipertexto)</span>
+                <div className='skill-html'>
+                  <a href="#"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" alt="Logo Html" /></a>
+                  <span>Linguagem de Marcação de Hipertexto</span>
+                  </div>
+            </div>
+            <div className='skill'>
+                <div className='skill-html'>
+                  <a href="#"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" alt="Logo Html" /></a>
+                    <span>Linguagem de estilo em cascata</span>
+                  </div>
+            </div>
+            <div className='skill'>
+                <div className='skill-html'>
+                  <a href="#"><img src="https://handlebarsjs.com/images/handlebars_logo.png" alt="Logo Html" /></a>
+                    <span>linguagem de modelagem de templates semântica minimalista</span>
+                  </div>
             </div>
         </div>
     </div>
