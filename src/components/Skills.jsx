09987e5745690f2793cfd7b745a5bb3e55effea8
patricklohn <patricklohn3@gmail.com>
2025-03-22 08:52:48 -0300

    estrutura de skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,25 +3,101 @@ import "./Skills.css"
 
 const Skills = () => {
   return (
-    <div>
-        <h1>MySkills</h1>
+    <div className='skill-main'>
+        <h1>MySkills Developer</h1>
+        <span>Linguagens estilização e marcação</span>
         <div className='container-skills'>
             <div className='skill'>
                 <div className='skill-html'>
                   <a href="#"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" alt="Logo Html" /></a>
-                  <span>Linguagem de Marcação de Hipertexto</span>
+                  <span>HTML</span>
                   </div>
             </div>
             <div className='skill'>
                 <div className='skill-html'>
                   <a href="#"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" alt="Logo Html" /></a>
-                    <span>Linguagem de estilo em cascata</span>
+                    <span>CSS</span>
                   </div>
             </div>
             <div className='skill'>
                 <div className='skill-html'>
                   <a href="#"><img src="https://handlebarsjs.com/images/handlebars_logo.png" alt="Logo Html" /></a>
-                    <span>linguagem de modelagem de templates semântica minimalista</span>
+                    <span>Handlebars</span>
+                  </div>
+            </div>
+        </div>
+        <span>Frameworks</span>
+        <div className='container-skills'>
+            <div className='skill'>
+                <div className='skill-html'>
+                  <a href="#"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg" alt="Logo Html" /></a>
+                    <span>React</span>
+                  </div>
+            </div>
+            <div className='skill'>
+                <div className='skill-html'>
+                  <a href="#"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-plain-wordmark.svg" alt="Logo Html" /></a>
+                    <span>Node.js</span>
+                  </div>
+            </div>
+        </div>
+        <span>Linguagens de programação e banco de dados</span>
+        <div className='container-skills'>
+            <div className='skill'>
+                <div className='skill-html'>
+                  <a href="#"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg" alt="Logo Html" /></a>
+                    <span>JavaScript</span>
+                  </div>
+            </div>
+            <div className='skill'>
+                <div className='skill-html'>
+                  <a href="#"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-plain.svg" alt="Logo Html" /></a>
+                    <span>TypeScript</span>
+                  </div>
+            </div>
+            <div className='skill'>
+                <div className='skill-html'>
+                  <a href="#"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="Logo Html" /></a>
+                    <span>Python</span>
+                  </div>
+            </div>
+            <div className='skill'>
+                <div className='skill-html'>
+                  <a href="#"><img src="https://s3.us-west-2.amazonaws.com/content.podia.com/iiu3hb9rvquyj4iazcz8rhivdddr" alt="Logo Html" /></a>
+                    <span>SQL</span>
+                  </div>
+            </div>
+        </div>
+        <span>Banco de dados e ORMs</span>
+        <div className='container-skills'>
+            <div className='skill'>
+                <div className='skill-html'>
+                  <a href="#"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr6dZwUF_v93lrAZ-iRpQif07ek60KiWi6Jw&s" alt="Logo Html" /></a>
+                    <span>MySql</span>
+                  </div>
+            </div>
+            <div className='skill'>
+                <div className='skill-html'>
+                  <a href="#"><img src="https://miro.medium.com/v2/resize:fit:512/1*doAg1_fMQKWFoub-6gwUiQ.png" alt="Logo Html" /></a>
+                    <span>MongoDb</span>
+                  </div>
+            </div>
+            <div className='skill'>
+                <div className='skill-html'>
+                  <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png" alt="Logo Html" /></a>
+                    <span>PostgreSQL</span>
+                  </div>
+            </div>
+            <div className='skill'>
+                <div className='skill-html'>
+                  <a href="#"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbaa90od4jPrZxQ_Rbo721GYTiR3ESbwZinQ&s" alt="Logo Html" /></a>
+                    <span>Sequelize</span>
+                  </div>
+            </div>
+            <div className='skill'>
+                <div className='skill-html'>
+                  <a href="#"><img src="https://media2.dev.to/dynamic/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2F11d2p2uhd8628whcoxuq.png" alt="Logo Html" /></a>
+                    <span>Prisma</span>
                   </div>
             </div>
         </div>
